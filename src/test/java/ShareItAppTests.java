ed08417bb8ec80ca35c1b0026c6353010ca97ba1
Misery-aripes <aaaa_wwww555@mail.ru>
2025-03-05 02:19:44 +0500

    fix tests

--- src/test/java/ShareItAppTests.java
+++ src/test/java/ShareItAppTests.java
@@ -2,7 +2,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
 
-@SpringBootTest(classes = ru.practicum.shareit.ShareItApp.class, webEnvironment = SpringBootTest.WebEnvironment.NONE)
+@SpringBootTest
 @TestPropertySource(locations = "classpath:application-test.properties")
 class ShareItAppTests {
     @Test
