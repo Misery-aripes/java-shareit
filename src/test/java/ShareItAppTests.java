e33dbb1c13dee385dc83280d3da2974c282bb1e7
Misery-aripes <aaaa_wwww555@mail.ru>
2025-03-05 02:18:04 +0500

    fix tests

--- src/test/java/ShareItAppTests.java
+++ src/test/java/ShareItAppTests.java
@@ -4,7 +4,7 @@ import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest(classes = ru.practicum.shareit.ShareItApp.class, webEnvironment = SpringBootTest.WebEnvironment.NONE)
 @TestPropertySource(locations = "classpath:application-test.properties")
-class ShareItApp {
+class ShareItAppTests {
     @Test
     void contextLoads() {
     }
