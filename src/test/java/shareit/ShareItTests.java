722efac91891d47f580bab659c18940b23f13a0c
Misery-aripes <aaaa_wwww555@mail.ru>
2025-03-04 23:06:05 +0500

    fix tests

--- src/test/java/shareit/ShareItTests.java
+++ src/test/java/shareit/ShareItTests.java
@@ -2,8 +2,9 @@ package shareit;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import ru.practicum.shareit.ShareItApp;
 
-@SpringBootTest
+@SpringBootTest(classes = ShareItApp.class)
 class ShareItTests {
 
 	@Test
