f293054d1a66d9727c2ee4d145b5d6ef048b7eda
Misery-aripes <aaaa_wwww555@mail.ru>
2025-03-05 21:53:50 +0500

    fix test

--- src/test/java/ShareItAppTests.java
+++ src/test/java/ShareItAppTests.java
@@ -1,8 +1,6 @@
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 
-@ActiveProfiles("test")
 @SpringBootTest
 class ShareItAppTests {
     @Test
